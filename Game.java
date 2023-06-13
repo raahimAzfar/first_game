219e815bdae9b55aa92ccb96c1ddc6ad23c55a4b
raahimAzfar <135705618+raahimAzfar@users.noreply.github.com>
2023-06-13 18:23:34 +0300

    Add files via upload
    My first upload

---
+++ Game.java
@@ -0,0 +1,33 @@
+package FirstGame;
+
+import java.awt.Color;
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
+public class Game {
+    // Defines the window
+    JFrame window;
+    Container con;
+    public static void main(String[] args) {
+        // Initializes the game
+        new Game();
+    }
+
+    public Game() {
+
+        // Creates a window
+        window = new JFrame();
+        // Sets the size of the window
+        window.setSize(800, 600);
+        // Allows you to close the window
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Sets the background color of the window
+        window.getContentPane().setBackground(Color.black);
+        // Disable default layout
+        window.setLayout(null);
+        // Makes the window visible
+        window.setVisible(true);
+    }
+
+}
