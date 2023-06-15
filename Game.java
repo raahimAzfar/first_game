d13fcd0ad1ba6308d5b1c90b8a7e6ff850a1c1bd
Raahim Azfar <raahimazfar@outlook.com>
2023-06-15 09:21:10 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -4,20 +4,21 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 
 public class Game {
     // Defines the window, container, title panel, and title.
-    JFrame titleWindow;
+    JFrame window;
     Container con;
-    JPanel titlePanel, startButtonPanel;
-    JLabel titleLabel;
+    JPanel titleNamePanel, startButtonPanel;
+    JLabel titleNameLabel;
     JButton startButton;
-    Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+	Font normalFont = new Font("Times New Roman", Font.PLAIN, 28);
 
     public static void main(String[] args) {
         // Initializes the game
@@ -30,42 +31,45 @@ public class Game {
         Creates a window, sets the size, allows you to close it, disables default layout,
         and makes it visible
         */
-        titleWindow = new JFrame();
-        titleWindow.setSize(800, 600);
-        titleWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        titleWindow.getContentPane().setBackground(Color.black);
-        titleWindow.setLayout(null);
-        titleWindow.setVisible(true);
+        window = new JFrame();
+		window.setSize(800, 600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setBackground(Color.black);
+		window.setLayout(null);
 
         // Makes container
-        con = titleWindow.getContentPane();
+        con = window.getContentPane();
 
         // Creates a new JPanel, sets it's bounds and it's color.
-        titlePanel = new JPanel();
-        titlePanel.setBounds(100, 100, 600, 150);
-        titlePanel.setBackground(Color.black);
+        titleNamePanel = new JPanel();
+		titleNamePanel.setBounds(100, 100, 600, 150);
+		titleNamePanel.setBackground(Color.black);
 
-        // Creates a title.
-        titleLabel = new JLabel("My first game");
-        titleLabel.setForeground(Color.white);
-        titleLabel.setFont(titleFont);
+        // Creates a title
+		titleNameLabel = new JLabel("ADVENTURE");
+		titleNameLabel.setForeground(Color.white);
+		titleNameLabel.setFont(titleFont);	
 
         // Creates a panel for the start button.
         startButtonPanel = new JPanel();
-        startButtonPanel.setBounds(300, 400, 200, 100);
-        startButtonPanel.setBackground(Color.black);
+		startButtonPanel.setBounds(300, 400, 200, 100);
+		startButtonPanel.setBackground(Color.black);
 
         // Creates the start button.
         startButton = new JButton("START");
-        startButton.setBackground(Color.black);
-        startButton.setForeground(Color.white);
-        startButton.setFont(normalFont);
+		startButton.setBackground(Color.black);
+		startButton.setForeground(Color.white);
+		startButton.setFont(normalFont);
+		startButton.setFocusPainted(false);
 
         // Adds everything to the window
-        titlePanel.add(titleLabel);
-        startButtonPanel.add(startButton);
-        con.add(titlePanel);
-        con.add(startButtonPanel);
+        titleNamePanel.add(titleNameLabel);
+		startButtonPanel.add(startButton);
+		
+		con.add(titleNamePanel);
+		con.add(startButtonPanel);
+		
+		window.setVisible(true);
     }
 
 }
